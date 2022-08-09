263b87b1a1186f4abdb3d6fc3b3a18df2c618a77
ekubay <axuTec14@gmail.com>
2022-08-09 13:42:46 +0300

    fixing the find_friends_count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -80,13 +80,13 @@ class TweetDfExtractor:
     
     # 
     def find_followers_count(self)->list:
-        followers_count = [tweet['followers_count'] for tweet in self.tweets_list]
+        followers_count = [tweet['user']['followers_count'] for tweet in self.tweets_list]
         
         return followers_count
 
     # 
     def find_friends_count(self)->list:
-        friends_count = [x['user']['friends_count'] for x in self.tweets_list]
+        friends_count = [tweet['user']['fi']]
         
         return friends_count
 
