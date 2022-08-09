3bf7092bea36f35b342e251a241da3532c156804
ekubay <axuTec14@gmail.com>
2022-08-09 16:05:32 +0300

    fixing drop_duplicates function

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -24,7 +24,7 @@ class Clean_Tweets:
         drop duplicate rows
         """
         
-        #df.drop_duplicates(inplace = True)
+        df.drop_duplicates(inplace = True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
@@ -64,4 +64,5 @@ if __name__ == "__main__":
         #_, tweet_list = read_json("global_twitter_data.json")
         df = pd.read_csv('processed_tweet_data.csv')
         df = Clean_Tweets(df)
+
         #tweet_df = tweet.get_tweet_df(save = True)
